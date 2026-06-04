e563dbe668072459071977ad8f7e5c6129f8a421
mtbtv <mtbtv.official.2@gmail.com>
2026-06-04 22:12:41 +0530

    Create intex.tsx

---
+++ app/intex.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import RootNavigator from './navigation/RootNavigator';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
+  );
+      }
