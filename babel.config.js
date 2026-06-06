5fe2f75b3b20ef197ed8550c6c795ce775c10362
mtbtv <mtbtv.official.2@gmail.com>
2026-06-06 11:22:18 +0000

    Fix: Overwrite babel config with clean template

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['react-native-worklets-core/plugin'babel-preset-expo'react-native-worklets-core/plugin', { jsxImportSource: 'react-native-worklets-core/plugin'nativewind'react-native-worklets-core/plugin' }],
-      'react-native-worklets-core/plugin'nativewind/babel'react-native-worklets-core/plugin',
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
-        'react-native-worklets-core/plugin'module-resolver'react-native-worklets-core/plugin',
+        'module-resolver',
         {
+          root: ['./'],
           alias: {
-            'react-native-worklets-core/plugin'@'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app'react-native-worklets-core/plugin',
-            'react-native-worklets-core/plugin'@screens'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/screens'react-native-worklets-core/plugin',
-            'react-native-worklets-core/plugin'@components'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/components'react-native-worklets-core/plugin',
-            'react-native-worklets-core/plugin'@hooks'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/hooks'react-native-worklets-core/plugin',
-            'react-native-worklets-core/plugin'@utils'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/utils'react-native-worklets-core/plugin',
-            'react-native-worklets-core/plugin'@types'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/types'react-native-worklets-core/plugin',
-            'react-native-worklets-core/plugin'@store'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/store'react-native-worklets-core/plugin',
-            'react-native-worklets-core/plugin'@providers'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/providers'react-native-worklets-core/plugin',
+            '@': './',
           },
         },
       ],
-      'react-native-worklets-core/plugin'react-native-reanimated/plugin'react-native-worklets-core/plugin',
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+      'react-native-worklets-core/plugin',
     ],
   };
 };
