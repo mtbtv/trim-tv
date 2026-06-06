fe38f11bc9a46e1087103416b4b7d381734e6f82
mtbtv <mtbtv.official.2@gmail.com>
2026-06-06 13:15:41 +0000

    Fix: Hard reset babel config

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,10 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-          },
-        },
-      ],
       'nativewind/babel',
       'react-native-reanimated/plugin',
-      'react-native-worklets-core/plugin',
     ],
   };
 };
