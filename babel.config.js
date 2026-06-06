2984ef983663abfe60fcc118ce94c26c1728fdcf
mtbtv <mtbtv.official.2@gmail.com>
2026-06-06 10:45:43 +0000

    Final Fix: Successfully synchronized packages and babel config

--- babel.config.js
+++ babel.config.js
@@ -2,26 +2,26 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
+      ['react-native-worklets-core/plugin'babel-preset-expo'react-native-worklets-core/plugin', { jsxImportSource: 'react-native-worklets-core/plugin'nativewind'react-native-worklets-core/plugin' }],
+      'react-native-worklets-core/plugin'nativewind/babel'react-native-worklets-core/plugin',
     ],
     plugins: [
       [
-        'module-resolver',
+        'react-native-worklets-core/plugin'module-resolver'react-native-worklets-core/plugin',
         {
           alias: {
-            '@': './app',
-            '@screens': './app/screens',
-            '@components': './app/components',
-            '@hooks': './app/hooks',
-            '@utils': './app/utils',
-            '@types': './app/types',
-            '@store': './app/store',
-            '@providers': './app/providers',
+            'react-native-worklets-core/plugin'@'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app'react-native-worklets-core/plugin',
+            'react-native-worklets-core/plugin'@screens'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/screens'react-native-worklets-core/plugin',
+            'react-native-worklets-core/plugin'@components'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/components'react-native-worklets-core/plugin',
+            'react-native-worklets-core/plugin'@hooks'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/hooks'react-native-worklets-core/plugin',
+            'react-native-worklets-core/plugin'@utils'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/utils'react-native-worklets-core/plugin',
+            'react-native-worklets-core/plugin'@types'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/types'react-native-worklets-core/plugin',
+            'react-native-worklets-core/plugin'@store'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/store'react-native-worklets-core/plugin',
+            'react-native-worklets-core/plugin'@providers'react-native-worklets-core/plugin': 'react-native-worklets-core/plugin'./app/providers'react-native-worklets-core/plugin',
           },
         },
       ],
-      'react-native-reanimated/plugin',
+      'react-native-worklets-core/plugin'react-native-reanimated/plugin'react-native-worklets-core/plugin',
     ],
   };
 };
